a2335f6931880070ab294ae152f2dcf2b8f127e5
rodgreg02 <mindera@ME00391.local>
2023-10-19 22:14:01 +0100

    Further developed game logic, added ASCII art made by yours truly(To be worked on), added punishment for more than one char. Arrays changed back to chars cuz easier

--- src/Main.java
+++ src/Main.java
@@ -7,27 +7,58 @@ public class Main{
         String word = getRandomWord();
         char[] charArray = new char[word.length()];
         for (int i = 0; i < word.length(); i++) {
-            charArray[i] = word.charAt(i);
-        }
-        render(charArray);
 
+          charArray[i]  = word.charAt(i);
+        }
+
+        String hiddenWord = render(charArray, word);
+        System.out.println(hiddenWord);
     }
     //Used to pick a random word from the pre defined array of words
     public static String getRandomWord(){
         String[] words = {"comunicar","roto","maravilha","milagroso","olhar","cadeira","lugares","escada","janela","cabelo"};
         Random random = new Random();
-        return words[random.nextInt(0,9)];
+        return words[random.nextInt(0,10)];
     }
-
-    public static char[] render(char[] wordArray, char a){
+//Holds major part of game logic and renders game board
+    public static String render(char[] wordArray, String pickedWord){
         char[] hiddenWord = new char[wordArray.length];
+        boolean wordPicked = false;
         for (int i = 0; i < wordArray.length; i++) {
             hiddenWord[i] = '_';
         }
 
-        for (int i = 0; i < hiddenWord.length; i++) {
-            System.out.print(hiddenWord[i] + " ");
+        while(!wordPicked) {
+            for (int i = 0; i < hiddenWord.length; i++) {
+                System.out.print(hiddenWord[i] + " ");
+            }
+
+            System.out.println("Input your choice of one character ONLY:");
+            Scanner input = new Scanner(System.in);
+            String userInput = input.nextLine();
+            if(userInput.length() > 1){
+                System.out.println("You absolute wet towel, can't you follow simple instructions?\n you'll hang for that.");
+                System.out.println("___\n  |\n  |\n  O \n (|)" +" \n /|");
+                String wetWipe = "You wetwipe";
+                return wetWipe;
+            }
+
+
+
+
+
+            char pickedChar = userInput.charAt(0);
+
+            for (int i = 0; i < wordArray.length; i++) {
+                if (pickedChar == (wordArray[i])){
+                    int indexOfLetter = i;
+                    hiddenWord[i] = pickedChar;
+                    continue;
+
+                }
+            }
+
+
         }
-        return null;
-    }
+        return pickedWord;}
 }
